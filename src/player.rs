d6302283e84e9c9d4ad1554677f687f2df6a83c4
lonnonjamesd <lemonjameslem@protonmail.com>
2022-02-26 00:37:28 -0500

    Movement

--- src/player.rs
+++ src/player.rs
@@ -1,9 +1,11 @@
 use bevy::{prelude::{Component, Transform, Query, Res, KeyCode}, input::Input};
 
+use crate::constants::DELTA_TIME;
+
 #[derive(Component)]
 pub struct Player {
     pub name: String,
-    pub speed: u32,
+    pub speed: f32,
 }
 
 
@@ -11,24 +13,26 @@ pub fn player_movement_system(
     keyboard_input: Res<Input<KeyCode>>,
     mut query: Query<(&Player, &mut Transform)>
 ) {
-    let (player, p_transform) = query.single_mut();
+    let (player, mut p_transform) = query.single_mut();
 
-    let mut x_mov = 0;
-    let mut z_mov = 0;
+    let mut x_mov = 0.0;
+    let mut z_mov = 0.0;
 
 
     if keyboard_input.pressed(KeyCode::W) {
-        x_mov += 1;
+        x_mov += 1.0;
     }
     if keyboard_input.pressed(KeyCode::S) {
-        x_mov -= 1;
+        x_mov -= 1.0;
     }
     if keyboard_input.pressed(KeyCode::A) {
-        z_mov += 1;
+        z_mov += 1.0;
     }
     if keyboard_input.pressed(KeyCode::D) {
-        z_mov -= 1;
+        z_mov -= 1.0;
     }
 
-    
+    let mut p_translation = p_transform.translation;
+    p_translation.x += x_mov * player.speed * DELTA_TIME;
+    p_translation.z += z_mov * player.speed * DELTA_TIME;
 }
