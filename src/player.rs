bb251013991957eafa16a57f259f7ee0e1d20f13
lonnonjamesd <lemonjameslem@protonmail.com>
2022-02-26 00:25:05 -0500

    movement

--- src/player.rs
+++ src/player.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::{Component, Transform, Query};
+use bevy::{prelude::{Component, Transform, Query, Res, KeyCode}, input::Input};
 
 #[derive(Component)]
 pub struct Player {
@@ -8,7 +8,27 @@ pub struct Player {
 
 
 pub fn player_movement_system(
+    keyboard_input: Res<Input<KeyCode>>,
     mut query: Query<(&Player, &mut Transform)>
 ) {
+    let (player, p_transform) = query.single_mut();
+
+    let mut x_mov = 0;
+    let mut z_mov = 0;
+
+
+    if keyboard_input.pressed(KeyCode::W) {
+        x_mov += 1;
+    }
+    if keyboard_input.pressed(KeyCode::S) {
+        x_mov -= 1;
+    }
+    if keyboard_input.pressed(KeyCode::A) {
+        z_mov += 1;
+    }
+    if keyboard_input.pressed(KeyCode::D) {
+        z_mov -= 1;
+    }
+
     
 }
