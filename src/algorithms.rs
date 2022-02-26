1ae8732e101f649236564e1b4791afefeb0bb736
wahfl2 <wahflbeljihn@gmail.com>
2022-02-26 14:22:24 -0600

    Adding pipes, moved pipe id to constants

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -9,15 +9,22 @@ pub struct Pipe<T, U, V, W> {
     c2: Connection<U>,
     c3: Connection<V>,
     c4: Connection<W>,
+    id: u32
 }
 
 impl Pipe<T, U, V, W> {
     pub fn new(con_1: Connection, con_2: Connection, con_3: Connection, con_4: Connection) -> Pipe {
+        GLOBAL_PIPE_ID += 1;
         Pipe {
             c1: con_1,
             c2: con_2,
             c3: con_3,
             c4: con_4,
+            id: GLOBAL_PIPE_ID
         }
     }
-} 
+
+    pub fn connected_to(self, goal_id: u32) -> bool {
+
+    }
+}
--- src/constants.rs
+++ src/constants.rs
@@ -1,2 +1,3 @@
 pub const DELTA_TIME: f32 = 1.0 / 60.0;
 pub const SQRT_OF_2: f32 = 1.41421356237f32 / 2.0;
+static mut GLOBAL_PIPE_ID: u32 = 0;
