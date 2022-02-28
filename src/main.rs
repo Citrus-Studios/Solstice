1aee4e4bcebc7a29dfd8291824e175757b53c516
lonnonjamesd <lemonjameslem@protonmail.com>
2022-02-27 23:17:19 -0500

    Player has a collider now

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::f32::consts::PI;
+
 use bevy::{prelude::*, core::FixedTimestep};
 use bevy_obj::ObjPlugin;
 use constants::DELTA_TIME;
+use heron::{PhysicsLayer, PhysicsPlugin, CollisionShape, RigidBody, Gravity, AxisAngle, Velocity};
 use player::{Player, player_movement_system, CameraComp, player_camera_system};
 use terrain_generation_system::generator::{GeneratorOptions, generate_terrain};
 
@@ -15,11 +18,13 @@ fn main() {
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .add_plugin(ObjPlugin)
+        .add_plugin(PhysicsPlugin::default())
         .add_startup_system(setup)
         .insert_resource(GeneratorOptions {
             width: 10,
             height: 1,
         })
+        .insert_resource(Gravity::from(Vec3::new(0.0, -1.0, 0.0))) // gravity
         .add_startup_system(generate_terrain)
         .add_system_set(
             SystemSet::new()
@@ -30,12 +35,26 @@ fn main() {
         .run();
 }
 
-/// set up a simple 3D scene
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..Default::default()
+    }).insert(CollisionShape::Cuboid {
+        half_extends: Vec3::new(1.0, 1.0, 1.0),
+        border_radius: None,
+    })
+    .insert(
+        Velocity::from_linear(Vec3::X * 10.0)
+            .with_angular(AxisAngle::new(Vec3::Z, 0.5 * PI))
+    );
+
+
     // player 
     commands.spawn_bundle(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
@@ -46,6 +65,11 @@ fn setup(
     .insert(Player {
         name: "None".to_string(),
         speed: 10.0
+    })
+    .insert(RigidBody::Dynamic)
+    .insert(CollisionShape::Cuboid {
+        half_extends: Vec3::new(1.0, 1.0, 1.0),
+        border_radius: None,
     })
     .with_children(|child| {
         // camera
