5f1bba9854c27fd505736bc1d281a45e96cfb10c
lonnonjamesd <lemonjameslem@protonmail.com>
2022-03-03 17:25:31 -0500

    Changed minor things

--- src/terrain_generation_system/generator.rs
+++ src/terrain_generation_system/generator.rs
@@ -31,19 +31,21 @@ pub fn generate_terrain(
 
     commands.spawn_bundle(PbrBundle {
         mesh: hollowground_handle,
-        material: materials.add(Color::rgb(randomcolor.0, randomcolor.1, randomcolor.2).into()),
+        material: materials.add(StandardMaterial {
+            base_color: Color::rgb(randomcolor.0, randomcolor.1, randomcolor.2),
+            ..Default::default()
+        }),
         transform: Transform::from_xyz(0.0, -2.0, 0.0),
         ..Default::default()
     })
     .insert_bundle(ColliderBundle {
         shape: ColliderShape::compound(vec![(
-            [0.0, -1.0, 0.0].into(), 
+            [0.0, -2.0, 0.0].into(), 
             ColliderShape::cuboid(3.0, 1.0, 3.0))]
         ).into(),
         position: [0.0, -2.0, 0.0].into(),
         ..Default::default()
     })
     .insert(ColliderPositionSync::Discrete)
-    .insert(ColliderDebugRender::with_id(1))
     .insert(RayCastMesh::<RaycastSet>::default());
 }
