1b9be29a2032758911a8135609d404d7888f1478
lonnonjamesd <lemonjameslem@protonmail.com>
2022-03-03 21:34:24 -0500

    Build system

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
-cargo build --release
-strip ./target/release/eclipsis
-mv ./target/release/eclipsis ./solstice
-zip -q ./solstice.zip ./solstice ./assets/models/*/*.obj
+if [ $1 == "windows" ]; then
+    cargo build --release --target=x86_64-pc-windows-gnu;
+    strip ./target/x86_64-pc-windows-gnu/release/eclipsis.exe;
+    mv ./target/x86_64-pc-windows-gnu/release/eclipsis.exe ./solstice.exe;
+    zip -q ./solstice-windows.zip ./solstice.exe ./assets/models/*/*.obj;
+else 
+    cargo build --release;
+    strip ./target/release/eclipsis;
+    mv ./target/release/eclipsis ./solstice;
+    zip -q ./solstice-linux.zip ./solstice ./assets/models/*/*.obj;
+fi
