58b8413574e2d43206b62832365325055fa87434
lonnonjamesd <lemonjameslem@protonmail.com>
2022-03-03 22:37:46 -0500

    Startup system for player

--- src/player_system/mod.rs
+++ src/player_system/mod.rs
@@ -1,7 +1,10 @@
-use bevy::{prelude::{Plugin, SystemSet}, core::FixedTimestep};
-use crate::{constants::DELTA_TIME};
+use crate::constants::DELTA_TIME;
+use bevy::{
+    core::FixedTimestep,
+    prelude::{Plugin, SystemSet},
+};
 
-use self::player::{player_movement_system};
+use self::player::player_movement_system;
 
 pub mod player;
 pub mod player_startup;
@@ -10,10 +13,11 @@ pub struct PlayerSystemPlugin;
 
 impl Plugin for PlayerSystemPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.add_system_set(
-            SystemSet::new()
-                .with_run_criteria(FixedTimestep::step(DELTA_TIME as f64))
-                .with_system(player_movement_system)
-        );
+        app.add_startup_system(player_startup::player_start)
+            .add_system_set(
+                SystemSet::new()
+                    .with_run_criteria(FixedTimestep::step(DELTA_TIME as f64))
+                    .with_system(player_movement_system),
+            );
     }
 }
