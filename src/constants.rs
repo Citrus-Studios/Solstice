d06c6adf74e4be0cd21601521737dd46253b8676
lonnonjamesd <lemonjameslem@protonmail.com>
2022-03-06 15:17:26 -0500

    some constant lol

--- src/constants.rs
+++ src/constants.rs
@@ -7,6 +7,7 @@ pub const DELTA_TIME: f32 = 1.0 / 60.0;
 pub const SQRT_OF_2: f32 = 1.41421356237f32 / 2.0;
 pub static mut GLOBAL_PIPE_ID: u32 = 0;
 pub const HALF_PI: f32 = PI / 2.0;
+pub const HALF_SIZE: f32 = 10.0;
 
 lazy_static! {
     pub static ref SEED: u32 = thread_rng().gen::<u32>();
