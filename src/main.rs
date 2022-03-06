15512fffe3abe44294f15c7845e5449254b6432e
lonnonjamesd <lemonjameslem@protonmail.com>
2022-03-05 20:42:29 -0500

    Gravity

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use bevy::{prelude::{App, Msaa}, DefaultPlugins};
 use bevy_mod_raycast::DefaultRaycastingPlugin;
 use bevy_obj::ObjPlugin;
-use bevy_rapier3d::{physics::{RapierPhysicsPlugin, NoUserData}, render::RapierRenderPlugin};
+use bevy_rapier3d::{physics::{RapierPhysicsPlugin, NoUserData, RapierConfiguration}, render::RapierRenderPlugin};
 use building_system::{RaycastSet, BuildingSystemPlugin};
 use player_system::PlayerSystemPlugin;
 use terrain_generation_system::GeneratorPlugin;
 
 pub mod building_system;
 pub mod player_system;
-pub mod terrain_generation_system;
+pub mod terrain_generation_system; 
 
 pub mod constants;
 
@@ -23,5 +23,9 @@ fn main() {
         .add_plugin(GeneratorPlugin)
         .add_plugin(BuildingSystemPlugin)
         .add_plugin(PlayerSystemPlugin)
+        .insert_resource(RapierConfiguration {
+            gravity: [0.0, -9.81, 0.0].into(),
+            ..Default::default()
+        })
         .run();
 }
