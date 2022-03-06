2c47155bb3d10c186a2189d21057285164bdc1d5
lonnonjamesd <lemonjameslem@protonmail.com>
2022-03-06 12:11:50 -0500

    Merged Stash and Main

--- src/terrain_generation_system/generator.rs
+++ src/terrain_generation_system/generator.rs
@@ -1,11 +1,10 @@
-use std::sync::Arc;
 
 use bevy::prelude::*;
 use bevy_mod_raycast::RayCastMesh;
-use bevy_rapier3d::{physics::{ColliderPositionSync, ColliderBundle}, prelude::{ColliderShape}};
+use bevy_rapier3d::{physics::{ColliderPositionSync, ColliderBundle}, prelude::{ColliderShape}, render::ColliderDebugRender};
 use rand::Rng;
 
-use noise::{NoiseFn, Perlin, Seedable, utils::{PlaneMapBuilder, NoiseMapBuilder}};
+use noise::{NoiseFn, Perlin, Seedable};
 
 use crate::{RaycastSet, constants::SEED};
 
@@ -24,12 +23,48 @@ pub fn generate_terrain(
     generator_options: Res<GeneratorOptions>,
 ) {
     let hollowground_handle = asset_server.load("models/ground1/ground1.obj");
+    let collider = ColliderBundle {
+        shape: ColliderShape::compound(vec![
+            ([0.0, -2.0, 0.0].into(),
+            ColliderShape::cuboid(3.0, 1.0, 3.0)),
+            ([0.0, 2.0, 0.0].into(),
+            ColliderShape::cuboid(3.0, 1.0, 3.0)),
+            ([-2.0, 0.0, 2.0].into(),
+            ColliderShape::cuboid(1.0, 1.0, 1.0)),
+            ([-2.0, 0.0, -2.0].into(),
+            ColliderShape::cuboid(1.0, 1.0, 1.0)),
+            ([2.0, 0.0, 2.0].into(),
+            ColliderShape::cuboid(1.0, 1.0, 1.0)),
+            ([2.0, 0.0, -2.0].into(),
+            ColliderShape::cuboid(1.0, 1.0, 1.0)),
+        ]).into(),
+        ..Default::default()
+    };
+
+    let collider2 = ColliderBundle {
+        shape: ColliderShape::compound(vec![
+            ([0.0, -2.0, 0.0].into(),
+            ColliderShape::cuboid(3.0, 1.0, 3.0)),
+            ([0.0, 2.0, 0.0].into(),
+            ColliderShape::cuboid(3.0, 1.0, 3.0)),
+            ([-2.0, 0.0, 2.0].into(),
+            ColliderShape::cuboid(1.0, 1.0, 1.0)),
+            ([-2.0, 0.0, -2.0].into(),
+            ColliderShape::cuboid(1.0, 1.0, 1.0)),
+            ([2.0, 0.0, 2.0].into(),
+            ColliderShape::cuboid(1.0, 1.0, 1.0)),
+            ([2.0, 0.0, -2.0].into(),
+            ColliderShape::cuboid(1.0, 1.0, 1.0)),
+        ]).into(),
+        ..Default::default()
+    };
 
     let mut rng = rand::thread_rng();
-    let randomcolor = (
+    let randomcolor = Color::rgba(
         rng.gen_range(0.0..255.0)/255.0, 
         rng.gen_range(0.0..255.0)/255.0, 
-        rng.gen_range(0.0..255.0)/255.0
+        rng.gen_range(0.0..255.0)/255.0,
+        0.1,
     );
 
     // let commands = Arc::new(Box::leak(Box::new(commands)));
@@ -53,7 +88,15 @@ pub fn generate_terrain(
                     transform: Transform::from_xyz((i as f32) * 3.0, -2.0, (j as f32) * 3.0),
                     ..Default::default()
                 })
-                .insert(RayCastMesh::<RaycastSet>::default());
+                    .insert(RayCastMesh::<RaycastSet>::default())
+                    .insert_bundle(collider)
+                    .insert(ColliderPositionSync::Discrete)
+                    .insert(RayCastMesh::<RaycastSet>::default());
+
+                commands.spawn_bundle(collider2)
+                    .insert(ColliderPositionSync::Discrete)
+                    .insert(ColliderDebugRender::with_id(1))
+                    .insert(RayCastMesh::<RaycastSet>::default());
             }
         }
     }
