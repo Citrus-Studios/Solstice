22143e4a75c3f6cbad723fd115f8984e20b8a815
lonnonjamesd <lemonjameslem@protonmail.com>
2022-03-06 13:01:05 -0500

    colliders wip

--- src/terrain_generation_system/generator.rs
+++ src/terrain_generation_system/generator.rs
@@ -23,41 +23,6 @@ pub fn generate_terrain(
     generator_options: Res<GeneratorOptions>,
 ) {
     let hollowground_handle = asset_server.load("models/ground1/ground1.obj");
-    let collider = ColliderBundle {
-        shape: ColliderShape::compound(vec![
-            ([0.0, -2.0, 0.0].into(),
-            ColliderShape::cuboid(3.0, 1.0, 3.0)),
-            ([0.0, 2.0, 0.0].into(),
-            ColliderShape::cuboid(3.0, 1.0, 3.0)),
-            ([-2.0, 0.0, 2.0].into(),
-            ColliderShape::cuboid(1.0, 1.0, 1.0)),
-            ([-2.0, 0.0, -2.0].into(),
-            ColliderShape::cuboid(1.0, 1.0, 1.0)),
-            ([2.0, 0.0, 2.0].into(),
-            ColliderShape::cuboid(1.0, 1.0, 1.0)),
-            ([2.0, 0.0, -2.0].into(),
-            ColliderShape::cuboid(1.0, 1.0, 1.0)),
-        ]).into(),
-        ..Default::default()
-    };
-
-    let collider2 = ColliderBundle {
-        shape: ColliderShape::compound(vec![
-            ([0.0, -2.0, 0.0].into(),
-            ColliderShape::cuboid(3.0, 1.0, 3.0)),
-            ([0.0, 2.0, 0.0].into(),
-            ColliderShape::cuboid(3.0, 1.0, 3.0)),
-            ([-2.0, 0.0, 2.0].into(),
-            ColliderShape::cuboid(1.0, 1.0, 1.0)),
-            ([-2.0, 0.0, -2.0].into(),
-            ColliderShape::cuboid(1.0, 1.0, 1.0)),
-            ([2.0, 0.0, 2.0].into(),
-            ColliderShape::cuboid(1.0, 1.0, 1.0)),
-            ([2.0, 0.0, -2.0].into(),
-            ColliderShape::cuboid(1.0, 1.0, 1.0)),
-        ]).into(),
-        ..Default::default()
-    };
 
     let mut rng = rand::thread_rng();
     let randomcolor = Color::rgba(
@@ -79,18 +44,54 @@ pub fn generate_terrain(
             let n = perlin.get([(i as f64) * 0.15, (j as f64) * 0.15]);
             //info!(n);
             if n > 0.0 {
+                let collider = ColliderBundle {
+                    shape: ColliderShape::compound(vec![
+                        ([0.0, -2.0, 0.0].into(),
+                        ColliderShape::cuboid(3.0, 1.0, 3.0)),
+                        ([0.0, 2.0, 0.0].into(),
+                        ColliderShape::cuboid(3.0, 1.0, 3.0)),
+                        ([-2.0 * (i as f32), 0.0, 2.0 * (j as f32)].into(),
+                        ColliderShape::cuboid(1.0, 1.0, 1.0)),
+                        ([-2.0 * (i as f32), 0.0, -2.0 * (j as f32)].into(),
+                        ColliderShape::cuboid(1.0, 1.0, 1.0)),
+                        ([2.0 * (i as f32), 0.0, 2.0 * (j as f32)].into(),
+                        ColliderShape::cuboid(1.0, 1.0, 1.0)),
+                        ([2.0 * (i as f32), 0.0, -2.0 * (j as f32)].into(),
+                        ColliderShape::cuboid(1.0, 1.0, 1.0)),
+                    ]).into(),
+                    ..Default::default()
+                };
+            
+                let collider2 = ColliderBundle {
+                    shape: ColliderShape::compound(vec![
+                        ([0.0, -2.0, 0.0].into(),
+                        ColliderShape::cuboid(3.0, 1.0, 3.0)),
+                        ([0.0, 2.0, 0.0].into(),
+                        ColliderShape::cuboid(3.0, 1.0, 3.0)),
+                        ([-2.0 * (i as f32), 0.0, 2.0 * (j as f32)].into(),
+                        ColliderShape::cuboid(1.0, 1.0, 1.0)),
+                        ([-2.0 * (i as f32), 0.0, -2.0 * (j as f32)].into(),
+                        ColliderShape::cuboid(1.0, 1.0, 1.0)),
+                        ([2.0 * (i as f32), 0.0, 2.0 * (j as f32)].into(),
+                        ColliderShape::cuboid(1.0, 1.0, 1.0)),
+                        ([2.0 * (i as f32), 0.0, -2.0 * (j as f32)].into(),
+                        ColliderShape::cuboid(1.0, 1.0, 1.0)),
+                    ]).into(),
+                    ..Default::default()
+                };
+
                 commands.spawn_bundle(PbrBundle {
                     mesh: hollowground_handle.clone(),
                     material: materials.add(StandardMaterial {
-                        base_color: Color::rgb(randomcolor.0, randomcolor.1, randomcolor.2),
+                        base_color: randomcolor,
                         ..Default::default()
                     }),
                     transform: Transform::from_xyz((i as f32) * 3.0, -2.0, (j as f32) * 3.0),
                     ..Default::default()
                 })
                     .insert(RayCastMesh::<RaycastSet>::default())
-                    .insert_bundle(collider)
                     .insert(ColliderPositionSync::Discrete)
+                    .insert_bundle(collider)
                     .insert(RayCastMesh::<RaycastSet>::default());
 
                 commands.spawn_bundle(collider2)
