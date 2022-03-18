9804dd132018b3101a0c99f11c6143b1a66faf11
wahfl2 <wahflbeljihn@gmail.com>
2022-03-18 17:30:08 -0500

    deleted gen_block function and replaced with new

--- src/terrain_generation_system/generator.rs
+++ src/terrain_generation_system/generator.rs
@@ -73,7 +73,7 @@ pub fn generate_terrain(
 
     info!("hewo?");
 
-    let hollowground_handle = asset_server.load("models/ground1/hollow_ground.obj");    
+    let hollowground_handle: Handle<Mesh> = asset_server.load("models/ground1/hollow_ground.obj");    
 
     // let commands = Arc::new(Box::leak(Box::new(commands)));
     // let hollowground_handle = &hollowground_handle;
@@ -88,50 +88,27 @@ pub fn generate_terrain(
     };
 
     let mesh_handle = meshes.add(handle_temp);
-    
-    // gen_block(
-    //     &mut commands,
-    //     hollowground_handle.clone(),
-    //     &mut materials,
-    //     meshes,
-    //     0.0,
-    //     0.0,
-    //     Vec3::new(0.0, 0.0, 0.0),
-    // );
+
+    let mut mesh = meshes.get(mesh_handle).unwrap().clone();
+    let hollowground_ref = meshes.get(&hollowground_handle).unwrap();
     
     // generates terrain given a width and a length
-    let mut terrain_gen = TerrainGenerator::new(
-        hollowground_handle,
-        &materials,
-        meshes.as_ref().clone(),
-        0.0,
-        0.0,
-        Vec3::ZERO,
-    );
-
     for i in 0..generator_options.width {
         for j in 0..generator_options.length {
             let n = perlin.get([(i as f64) * 0.15, (j as f64) * 0.15]);
             //info!(n);
             if n > 0.0 {
-                terrain_gen.i = i as f32;
-                terrain_gen.j = j as f32;
-                // let mut rng = rand::thread_rng();
-                // Generate a block
-                terrain_gen.offset.y = 0.0;
-                    gen_block(&mut terrain_gen);
+                    mesh = mesh.combine_mesh(hollowground_ref.clone(), Vec3::new((i as f32) * 3.0, 0.0, (j as f32) * 3.0));
+
                 if n >= 0.3 {
-                    terrain_gen.offset.y = -3.0;
-                    gen_block(&mut terrain_gen);
+                    mesh = mesh.combine_mesh(hollowground_ref.clone(), Vec3::new((i as f32) * 3.0, -3.0, (j as f32) * 3.0));
                 }
                 if n >= 0.6 {
-                    terrain_gen.offset.y = -6.0;
-                    gen_block(&mut terrain_gen);
+                    mesh = mesh.combine_mesh(hollowground_ref.clone(), Vec3::new((i as f32) * 3.0, -6.0, (j as f32) * 3.0));
                 }
 
                 if n >= 0.95 {
-                    terrain_gen.offset.y = -9.0;
-                    gen_block(&mut terrain_gen);
+                    mesh = mesh.combine_mesh(hollowground_ref.clone(), Vec3::new((i as f32) * 3.0, -9.0, (j as f32) * 3.0));
                 }
 
                 // Spires
@@ -152,47 +129,8 @@ pub fn generate_terrain(
             }
         }
     }
-    let positions = terrain_gen.positions.clone();
-    let normals = terrain_gen.normals.clone();
-    let uvs = terrain_gen.uvs.clone();
-    let indices = terrain_gen.indices.clone();
-    // let textures = terrain_gen.textures.clone();
-    // let colors = terrain_gen.colors.clone();
 
-    drop(terrain_gen);
-
-    let final_mesh = meshes.get_mut(mesh_handle).unwrap();
-
-    final_mesh.set_attribute(
-        "Vertex_Position",
-        VertexAttributeValues::Float32x3(positions),
-    );
-
-    final_mesh.set_attribute(
-        "Vertex_Normal",
-        VertexAttributeValues::Float32x3(normals),
-    );
-
-    final_mesh.set_attribute(
-        "Vertex_Uv",
-        VertexAttributeValues::Float32x2(uvs),
-    );
-
-    final_mesh.set_indices(Some(Indices::U32(indices)));
-
-    // final_mesh.set_attribute(
-    //     "Vertex_Texture",
-    //     VertexAttributeValues::Sint32x4(textures),
-    // );
-
-    // final_mesh.set_attribute(
-    //     "Vertex_Color",
-    //     VertexAttributeValues::Float32x4(colors),
-    // );
-
-    let final_mesh_clone = final_mesh.clone();
-    // info!("{:?}", &final_mesh_clone);
-    let final_mesh_handle = meshes.add(final_mesh_clone);
+    let final_mesh_handle = meshes.add(mesh);
 
     commands
         .spawn_bundle(PbrBundle {
@@ -207,89 +145,6 @@ pub fn generate_terrain(
     
     done.done = true;
 }
-
-fn gen_block(
-    terrain_gen: &mut TerrainGenerator
-) {
-    let hollowground_mesh = &mut terrain_gen.meshes.get(terrain_gen.hollowground_handle.clone()).unwrap().clone();
-
-    let mut hollowground_mesh_clone = hollowground_mesh.clone();
-
-    let hollowground_vertices = match hollowground_mesh_clone.attribute_mut("Vertex_Position").unwrap() {
-        bevy::render::mesh::VertexAttributeValues::Float32x3(e) => e,
-        _ => panic!("WHAT")
-    };
-
-    let mut hollowground_normals = match hollowground_mesh.clone().attribute("Vertex_Normal").unwrap() {
-        bevy::render::mesh::VertexAttributeValues::Float32x3(e) => e.to_vec(),
-        _ => panic!("WHAT")
-    };
-
-    let mut hollowground_uvs = match hollowground_mesh.clone().attribute("Vertex_Uv").unwrap() {
-        bevy::render::mesh::VertexAttributeValues::Float32x2(e) => e.to_vec(),
-        _ => panic!("WHAT")
-    };
-
-    let mut hollowground_indices = match hollowground_mesh.clone().indices().unwrap().clone() {
-        Indices::U32(e) => e,
-        _ => panic!("WHAT")
-    };
-
-    let mut final_vertices = Vec::new();
-
-    let vertices_len = hollowground_vertices.clone().len();
-
-    for vertex in hollowground_vertices {
-        final_vertices.push([
-            vertex[0] + ((terrain_gen.i as f32) * 3.0) + terrain_gen.offset.x, 
-            vertex[1] + (-2.0) + terrain_gen.offset.y, 
-            vertex[2] + ((terrain_gen.j as f32) * 3.0) + terrain_gen.offset.z
-        ]);
-    }
-
-    let hollowground_indices_len = hollowground_indices.len();
-
-    let len_cache = ((terrain_gen.indices.len() / hollowground_indices_len) * vertices_len) as u32;
-
-    for indice in hollowground_indices.iter_mut() {
-        *indice += len_cache;
-    }
-
-    terrain_gen.positions.append(&mut final_vertices);
-    terrain_gen.normals.append(&mut hollowground_normals);
-    terrain_gen.uvs.append(&mut hollowground_uvs);
-    terrain_gen.indices.append(&mut hollowground_indices);
-    // terrain_gen.textures.append(&mut hollowground_textures);
-    // terrain_gen.colors.append(&mut hollowground_colors);
-
-    // commands
-    //     .spawn_bundle(PbrBundle {
-    //         mesh: hollowground_handle.clone(),
-    //         material: materials.add(StandardMaterial {
-    //             base_color: Color::rgb(50.0 / 255.0, 56.0 / 255.0, 53.0 / 255.0),
-    //             ..Default::default()
-    //         }),
-    //         transform: Transform::from_xyz(
-    //             ((i as f32) * 3.0) + offset.x,
-    //             (-2.0) + offset.y,
-    //             ((j as f32) * 3.0) + offset.z,
-    //         )
-    //         //.with_scale(Vec3::new(0.5, 0.5, 0.5))
-    //         ,
-    //         ..Default::default()
-    //     })
-    //     .insert(RayCastMesh::<RaycastSet>::default())
-    //     .insert(ColliderPositionSync::Discrete)
-    //     .insert(RayCastMesh::<RaycastSet>::default())
-    //     .with_children(|parent| {
-    //         parent.spawn_bundle(ColliderBundle {
-    //             shape: ColliderShape::cuboid(1.5, 1.5, 1.5).into(),
-    //             position: (Vec3::new((i as f32) * 3.0, -2.0, (j as f32) * 3.0) + offset).into(),
-    //             ..Default::default()
-    //         });
-    //     });
-}
-
 
 fn gen_spire(
     commands: &mut Commands,
