a0cb59dac6d12fb4610ff0983bf8896fb07f6cea
wahfl2 <wahflbeljihn@gmail.com>
2022-03-20 03:09:32 -0500

    Changed player collider into rounded cuboid

--- src/player_system/player_startup.rs
+++ src/player_system/player_startup.rs
@@ -1,16 +1,25 @@
-use bevy::{prelude::{Mesh, Commands, ResMut, Assets, shape, Color, Transform, BuildChildren, PerspectiveCameraBundle}, pbr::{StandardMaterial, PbrBundle}};
+use bevy::{prelude::{Mesh, Commands, ResMut, Assets, shape, Color, Transform, BuildChildren, PerspectiveCameraBundle, AssetServer, Res, Handle}, pbr::{StandardMaterial, PbrBundle}};
 use bevy_mod_picking::RayCastSource;
 use bevy_rapier3d::{prelude::{RigidBodyType, ColliderShape, RigidBodyMassPropsFlags, ColliderMaterial, CoefficientCombineRule}, physics::{RigidBodyBundle, ColliderPositionSync, ColliderBundle}};
 
-use crate::building_system::RaycastSet;
+use crate::{building_system::RaycastSet, terrain_generation_system::generator::MutateMesh};
 
 use super::player::{Player, CameraComp};
+
+pub struct PlayerStartupDone {
+    pub done: bool
+}
 
 pub fn player_start(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    mut done: ResMut<PlayerStartupDone>
 ) {
+    if done.done {
+        return
+    }
+
     let lock_xyz_rotation = RigidBodyMassPropsFlags::ROTATION_LOCKED_Y
         | RigidBodyMassPropsFlags::ROTATION_LOCKED_Z
         | RigidBodyMassPropsFlags::ROTATION_LOCKED_X;
@@ -24,21 +33,21 @@ pub fn player_start(
     })
     .insert(Player {
         name: "None".to_string(),
-        speed: 300.0
-    })
-    .insert_bundle(RigidBodyBundle {
-        body_type: RigidBodyType::Dynamic.into(),
-        mass_properties: lock_xyz_rotation.into(),
-        ..Default::default()
+        speed: 2000.0
     })
     .insert_bundle(ColliderBundle {
-        shape: ColliderShape::cuboid(0.5, 0.5, 0.5).into(),
+        shape: ColliderShape::round_cuboid(0.5, 0.5, 0.5, 0.1).into(),
         position: [100.0, 20.0, 100.0].into(),
         material: ColliderMaterial { 
             friction: 0.0,
             friction_combine_rule: CoefficientCombineRule::Min,
             ..Default::default() 
         }.into(),
+        ..Default::default()
+    })
+    .insert_bundle(RigidBodyBundle {
+        body_type: RigidBodyType::Dynamic.into(),
+        mass_properties: lock_xyz_rotation.into(),
         ..Default::default()
     })
     .insert(ColliderPositionSync::Discrete)
@@ -56,4 +65,5 @@ pub fn player_start(
         .insert(RayCastSource::<RaycastSet>::new());
     });
 
+    done.done = true
 }
