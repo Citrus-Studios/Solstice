837c5dce335137d289cf2548540563e38ce6a848
wahfl2 <wahflbeljihn@gmail.com>
2022-04-20 23:33:26 -0500

    Added a macro for Building creation

--- src/building_system/buildings.rs
+++ src/building_system/buildings.rs
@@ -1,7 +1,7 @@
 use std::marker::PhantomData;
 
-use bevy::prelude::{Mesh, Handle};
-use bevy_rapier3d::prelude::SharedShape;
+use bevy::{prelude::{Mesh, Handle, Scene}, math::Vec3, pbr::StandardMaterial};
+use bevy_rapier3d::prelude::{SharedShape, ColliderShape};
 
 use crate::constants::GLOBAL_PIPE_ID;
 
@@ -35,8 +35,11 @@ pub enum BuildingIO {
 }
 
 pub struct BuildingShapeData {
-    pub mesh: Handle<Mesh>,
+    pub mesh: Option<Handle<Mesh>>,
+    pub material: Option<Handle<StandardMaterial>>,
+    pub path: String,
     pub collider: SharedShape,
+    pub collider_offset: Vec3,
 }
 
 pub enum Or<A, B> {
@@ -68,5 +71,69 @@ impl<T, U, V, W> Pipe<T, U, V, W> {
             c4,
             id: unsafe { GLOBAL_PIPE_ID },
         }
+    }
+}
+
+macro_rules! Building {
+    (
+        Type: $buildingtype:ident, 
+        Name: $name:literal, 
+        Flow: $flow:ident, 
+        Storage: $storage:expr, 
+        Current: $current:expr, 
+        Generation: $generation:expr,
+        Mesh: $mesh:expr,
+        Material: $material:expr,
+        MeshPath: $meshtype:literal, 
+        Collider: $collider:expr, 
+        Offset: ($x:literal, $y:literal, $z:literal)
+    ) => {
+        Building {
+            building_id: BuildingId {
+                building_type: BuildingType::$buildingtype,
+                building_name: $name.to_string(),
+            },
+            iridium_data: BuildingIridiumData {
+                io: BuildingIO::$flow,
+                storage: match $storage {
+                    -1 => None,
+                    _ => Some($storage as u32)
+                },
+                current:  match $current {
+                    -1 => None,
+                    _ => Some($current as u32)
+                },
+                generation:  match $generation {
+                    -1 => None,
+                    _ => Some($generation as u32)
+                },
+            },
+            shape_data: BuildingShapeData {
+                mesh: $mesh,
+                material: $material,
+                path: $meshtype.to_string(),
+                collider: $collider,
+                collider_offset: Vec3::new($x, $y, $z),
+            },
+        }
+    }
+}
+
+pub fn string_to_building(name: String) -> Building {
+    match name.as_str() {
+        "Well Pump" => Building!(
+            Type: Wellpump, 
+            Name: "Well Pump", 
+            Flow: InOut, 
+            Storage: 50, 
+            Current: 0, 
+            Generation: 5, 
+            Mesh: None, 
+            Material: None,
+            MeshPath: "models/buildings/well_pump.obj", 
+            Collider: ColliderShape::cylinder(1.129, 0.9), 
+            Offset: (0.0, 0.5645, 0.0)
+        ),
+        _ => panic!("Could not match {} to any building", name)
     }
 }
