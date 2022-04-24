e62f502bf209d71110b1aba1acafb88f280e5c07
wahfl2 <wahflbeljihn@gmail.com>
2022-04-24 14:27:51 -0500

    Finished writing model "loader"

--- src/model_loader.rs
+++ src/model_loader.rs
@@ -1,18 +1,32 @@
-use bevy::{pbr::PbrBundle, asset::{AssetPath, Asset}, prelude::*, gltf::{Gltf, GltfMesh, GltfPrimitive}};
+use bevy::{pbr::PbrBundle, prelude::*, gltf::GltfPrimitive, render::render_resource::PrimitiveTopology};
 
 use crate::{material_palette::MaterialPalette, terrain_generation_system::{relevant_attributes::RelevantAttributes, mutate_mesh::MutateMesh}};
 
-pub fn combine_gltf_mesh(primitives: Vec<GltfPrimitive>, meshes: Res<Assets<Mesh>>, materials: Res<Assets<StandardMaterial>>) -> PbrBundle {
-    let mut attr = RelevantAttributes::new();
+pub fn combine_gltf_mesh(primitives: Vec<GltfPrimitive>, meshes: &mut ResMut<Assets<Mesh>>, materials: &mut ResMut<Assets<StandardMaterial>>, images: &mut ResMut<Assets<Image>>) -> PbrBundle {
+    let mut attr_vec = Vec::new();
     let mut material_palette = MaterialPalette::new();
 
     for primitive in primitives {
         let mesh = meshes.get(primitive.mesh).unwrap().clone();
         let material = materials.get(primitive.material.unwrap()).unwrap().clone();
 
-        attr.append_with_indices(mesh.relevant_attributes());
+        attr_vec.push(mesh.relevant_attributes());
         material_palette.push(material.into());
     }
 
-    PbrBundle::default()
+    let textures = material_palette.compile(None);
+    let mut return_attr = RelevantAttributes::new();
+
+    for (i, mut attr) in attr_vec.into_iter().enumerate() {
+        attr.set_all_uv(textures.get_uv_pos(i as u32));
+        return_attr.append_with_indices(attr);
+    }
+
+    let mesh = meshes.add(Mesh::new(PrimitiveTopology::TriangleList).set_attributes(return_attr));
+
+    PbrBundle {
+        mesh,
+        material: materials.add(textures.into_standard_material(images)),
+        ..Default::default()
+    }
 }
