b716126f41b24419721afae25f9cefe56d275f4f
lonnonjamesd <lemonjameslem@protonmail.com>
2022-05-29 14:17:57 -0400

    Added basic code

--- solstice-server/src/main.rs
+++ solstice-server/src/main.rs
@@ -1,4 +1,15 @@
+use tokio::net::UdpSocket;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let socket = UdpSocket::bind("127.0.0.1:34254")
+        .await
+        .expect("Couldn't bind to that address");
+    let mut buf = [0; 10];
+    let (number_of_bytes, src_addr) = socket
+        .recv_from(&mut buf)
+        .await
+        .expect("Didn't receive data");
+    let filled_buf = &mut buf[..number_of_bytes];
+    println!("{:?}", filled_buf);
 }
