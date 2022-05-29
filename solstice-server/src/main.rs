faba73f49cfaf245d5a8ed3e7c54e705d2d73a7e
lonnonjamesd <lemonjameslem@protonmail.com>
2022-05-29 14:18:57 -0400

    Removed base code and added enet

--- solstice-server/src/main.rs
+++ solstice-server/src/main.rs
@@ -1,15 +1,2 @@
-use tokio::net::UdpSocket;
-
 #[tokio::main]
-async fn main() {
-    let socket = UdpSocket::bind("127.0.0.1:34254")
-        .await
-        .expect("Couldn't bind to that address");
-    let mut buf = [0; 10];
-    let (number_of_bytes, src_addr) = socket
-        .recv_from(&mut buf)
-        .await
-        .expect("Didn't receive data");
-    let filled_buf = &mut buf[..number_of_bytes];
-    println!("{:?}", filled_buf);
-}
+async fn main() {}
