ecc224f1d709a38e4191f7a413a91f3997f1e586
wahfl2 <wahflbeljihn@gmail.com>
2022-06-02 00:35:12 -0500

    Refactored with rustfmt

--- src/player_system/gui_system/gui.rs
+++ src/player_system/gui_system/gui.rs
@@ -1,14 +1,20 @@
-use crate::constants::{GUI_LOOKUP, GUI_BACK_LOOKUP};
+use crate::{
+    building_system::buildings::string_to_building_enum,
+    constants::{GUI_BACK_LOOKUP, GUI_LOOKUP},
+};
 
 use super::gui_startup::*;
-use bevy::{prelude::*, ecs::query::{QueryIter, WriteFetch, EntityFetch, ReadFetch}};
+use bevy::{
+    ecs::query::{EntityFetch, QueryIter, ReadFetch, WriteFetch},
+    prelude::*,
+};
 
 pub fn gui(
-    mut interaction_query: Query<
-        (&Interaction, &mut UiColor, &GuiButtonId),
-        Changed<Interaction>
-    >,
-    mut button_query: ParamSet<(Query<(&mut GuiButtons, Entity, &Children)>, Query<&GuiButtons>)>,
+    mut interaction_query: Query<(&Interaction, &mut UiColor, &GuiButtonId), Changed<Interaction>>,
+    mut button_query: ParamSet<(
+        Query<(&mut GuiButtons, Entity, &Children)>,
+        Query<&GuiButtons>,
+    )>,
     mut text_query: Query<(&mut Text, &GuiTextId, Entity)>,
     mut visibility_query: Query<&mut Visibility>,
     mut selected_branch: ResMut<GuiSelectedBranch>,
@@ -23,10 +29,23 @@ pub fn gui(
         match *interaction {
             Interaction::Clicked => {
                 clicked = true;
-                let clicked_button_content = &button_query.p1().iter().nth(button_id.id as usize).unwrap().content.clone();
+                let clicked_button_content = &button_query
+                    .p1()
+                    .iter()
+                    .nth(button_id.id as usize)
+                    .unwrap()
+                    .content
+                    .clone();
                 let mut button_query_q0 = button_query.p0();
                 let mut button_iter = button_query_q0.iter_mut();
-                click_button(clicked_button_content, &mut selected_branch, &mut text_query, &mut button_iter, &mut visibility_query, &mut selected_building);
+                click_button(
+                    clicked_button_content,
+                    &mut selected_branch,
+                    &mut text_query,
+                    &mut button_iter,
+                    &mut visibility_query,
+                    &mut selected_building,
+                );
             }
             Interaction::Hovered => {
                 *color = HOVERED_BUTTON.into();
@@ -50,18 +69,39 @@ pub fn gui(
         }
 
         if pressed_id >= 0 {
-            let clicked_button_content = &button_query.p1().iter().nth(pressed_id as usize).unwrap().content.clone();
+            let clicked_button_content = &button_query
+                .p1()
+                .iter()
+                .nth(pressed_id as usize)
+                .unwrap()
+                .content
+                .clone();
             let mut button_query_q0 = button_query.p0();
             let mut button_iter = button_query_q0.iter_mut();
-            click_button(clicked_button_content, &mut selected_branch, &mut text_query, &mut button_iter, &mut visibility_query, &mut selected_building);
+            click_button(
+                clicked_button_content,
+                &mut selected_branch,
+                &mut text_query,
+                &mut button_iter,
+                &mut visibility_query,
+                &mut selected_building,
+            );
         }
     }
 
     if keyboard_input.pressed(KeyCode::Q) && !prev_q.pressed {
         let mut button_query_q0 = button_query.p0();
         let mut button_iter = button_query_q0.iter_mut();
-        selected_branch.id = GUI_BACK_LOOKUP.get(&selected_branch.id).unwrap().to_string();
-        change_buttons(&selected_branch.id, &mut button_iter, &mut text_query, &mut visibility_query);
+        selected_branch.id = GUI_BACK_LOOKUP
+            .get(&selected_branch.id)
+            .unwrap()
+            .to_string();
+        change_buttons(
+            &selected_branch.id,
+            &mut button_iter,
+            &mut text_query,
+            &mut visibility_query,
+        );
     }
     prev_q.pressed = keyboard_input.pressed(KeyCode::Q);
 }
@@ -70,35 +110,56 @@ pub fn gui(
 pub enum GuiOr<T> {
     Id(String),
     Item(T),
-    None
+    None,
 }
 
 fn change_buttons(
-    branch_id: &String, 
-    button_iter: &mut QueryIter<(&mut GuiButtons, Entity, &Children), (WriteFetch<GuiButtons>, EntityFetch, ReadFetch<Children>), ()>, 
-    text_query: &mut Query<(&mut Text, &GuiTextId, Entity)>, 
-    visibility_query: &mut Query<&mut Visibility>
+    branch_id: &String,
+    button_iter: &mut QueryIter<
+        (&mut GuiButtons, Entity, &Children),
+        (WriteFetch<GuiButtons>, EntityFetch, ReadFetch<Children>),
+        (),
+    >,
+    text_query: &mut Query<(&mut Text, &GuiTextId, Entity)>,
+    visibility_query: &mut Query<&mut Visibility>,
 ) {
     let branch = GUI_LOOKUP.get(branch_id).unwrap();
 
     for (mut text, i, text_entity) in text_query.iter_mut() {
-        let button_content = &branch[i.id  as usize];
-        let (mut cur_button_content, cur_button_entity, cur_button_child) = button_iter.next().unwrap();
+        let button_content = &branch[i.id as usize];
+        let (mut cur_button_content, cur_button_entity, cur_button_child) =
+            button_iter.next().unwrap();
 
         visibility_query.get_mut(text_entity).unwrap().is_visible = true;
-        visibility_query.get_mut(cur_button_entity).unwrap().is_visible = true;
-        visibility_query.get_mut(cur_button_child[0]).unwrap().is_visible = true;
+        visibility_query
+            .get_mut(cur_button_entity)
+            .unwrap()
+            .is_visible = true;
+        visibility_query
+            .get_mut(cur_button_child[0])
+            .unwrap()
+            .is_visible = true;
         match &button_content {
             // Set the text in the button                                  Ignore everything before underscores
-            GuiOr::Id(e) => text.sections[0].value = e.to_string().rsplitn(2, "_").next().unwrap().to_string(),
-            GuiOr::Item(e) => text.sections[0].value = e.to_string().rsplitn(2, "_").next().unwrap().to_string(),
+            GuiOr::Id(e) => {
+                text.sections[0].value = e.to_string().rsplitn(2, "_").next().unwrap().to_string()
+            }
+            GuiOr::Item(e) => {
+                text.sections[0].value = e.to_string().rsplitn(2, "_").next().unwrap().to_string()
+            }
 
             // Hide the button
             GuiOr::None => {
                 visibility_query.get_mut(text_entity).unwrap().is_visible = false;
-                visibility_query.get_mut(cur_button_entity).unwrap().is_visible = false;
-                visibility_query.get_mut(cur_button_child[0]).unwrap().is_visible = false;
-            },
+                visibility_query
+                    .get_mut(cur_button_entity)
+                    .unwrap()
+                    .is_visible = false;
+                visibility_query
+                    .get_mut(cur_button_child[0])
+                    .unwrap()
+                    .is_visible = false;
+            }
         };
         cur_button_content.content = button_content.clone();
     }
@@ -108,7 +169,11 @@ fn click_button(
     clicked_button_content: &GuiOr<String>,
     selected_branch: &mut ResMut<GuiSelectedBranch>,
     text_query: &mut Query<(&mut Text, &GuiTextId, Entity)>,
-    button_iter: &mut QueryIter<(&mut GuiButtons, Entity, &Children), (WriteFetch<GuiButtons>, EntityFetch, ReadFetch<Children>), ()>,
+    button_iter: &mut QueryIter<
+        (&mut GuiButtons, Entity, &Children),
+        (WriteFetch<GuiButtons>, EntityFetch, ReadFetch<Children>),
+        (),
+    >,
     visibility_query: &mut Query<&mut Visibility>,
     selected_building: &mut ResMut<SelectedBuilding>,
 ) {
@@ -118,7 +183,7 @@ fn click_button(
             change_buttons(&e.to_string(), button_iter, text_query, visibility_query);
         }
         GuiOr::Item(e) => {
-            selected_building.id = Some(e.to_string());
+            selected_building.id = Some(string_to_building_enum(e.to_string()));
             selected_building.changed = true;
             info!("you selected {:?}!", e);
         }
--- src/player_system/gui_system/gui_startup.rs
+++ src/player_system/gui_system/gui_startup.rs
@@ -1,6 +1,9 @@
-use bevy::{prelude::*};
+use bevy::prelude::*;
 
-use crate::constants::{ShortToString, GUI_LOOKUP};
+use crate::{
+    building_system::buildings::BuildingType,
+    constants::{ShortToString, GUI_LOOKUP},
+};
 
 use super::gui::GuiOr;
 
@@ -8,37 +11,37 @@ pub const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
 pub const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
 
 pub struct GuiSelectedBranch {
-    pub id: String
+    pub id: String,
 }
 
 #[derive(Component)]
 pub struct GuiButtons {
     pub content: GuiOr<String>,
-    pub id: u32
+    pub id: u32,
 }
 
 #[derive(Component)]
 pub struct GuiButtonId {
-    pub id: u32
+    pub id: u32,
 }
 
 #[derive(Component)]
 pub struct GuiTextId {
-    pub id: u32
+    pub id: u32,
 }
 
 #[derive(Component)]
 pub struct GuiTextBox {
-    pub id: u32
+    pub id: u32,
 }
 
 pub struct PrevQPress {
-    pub pressed: bool
+    pub pressed: bool,
 }
 
 pub struct SelectedBuilding {
-    pub id: Option<String>,
-    pub changed: bool
+    pub id: Option<BuildingType>,
+    pub changed: bool,
 }
 
 pub fn gui_startup(mut commands: Commands, asset_server: Res<AssetServer>) {
@@ -48,7 +51,10 @@ pub fn gui_startup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_bundle(UiCameraBundle::default());
     commands.insert_resource(GuiSelectedBranch { id: "base".ts() });
     commands.insert_resource(PrevQPress { pressed: false });
-    commands.insert_resource(SelectedBuilding { id: None, changed: false });
+    commands.insert_resource(SelectedBuilding {
+        id: None,
+        changed: false,
+    });
     let branch = GUI_LOOKUP.get(&"base".ts()).unwrap();
     let mut margin = Rect::default();
     margin.left = Val::Px(14.0);
@@ -65,56 +71,69 @@ pub fn gui_startup(mut commands: Commands, asset_server: Res<AssetServer>) {
             },
             visibility: Visibility { is_visible: false },
             ..Default::default()
-        }).with_children(|parent| {
+        })
+        .with_children(|parent| {
             // spawn 4 buttons in the bundle as children
             for i in 0..=3 {
                 let mut margin = Rect::default();
                 let j = 3 - i;
                 margin.bottom = Val::Px(5.0);
-                parent.spawn_bundle(ButtonBundle {
-                    style: Style {
-                        size: Size::new(Val::Px(153.0), Val::Px(150.0)),
-                        margin,
-                        justify_content: JustifyContent::Center,
-                        align_items: AlignItems::Baseline,
-                        ..Default::default()
-                    },
-                    color: NORMAL_BUTTON.into(),
-                    ..Default::default()
-                }).insert(GuiButtons { content: branch[j].clone(), id: j as u32 })
-                .insert(GuiButtonId { id: 3 - j as u32 })
-                .with_children(|parent| {
-                    let mut margin = Rect::default();
-                    margin.bottom = Val::Px(114.0);
-                    parent.spawn_bundle(NodeBundle {
+                parent
+                    .spawn_bundle(ButtonBundle {
                         style: Style {
-                            size: Size::new(Val::Px(147.0), Val::Px(33.0)),
-                            margin: margin,
-                            // horizontally center child text
+                            size: Size::new(Val::Px(153.0), Val::Px(150.0)),
+                            margin,
                             justify_content: JustifyContent::Center,
-                            // vertically center child text
                             align_items: AlignItems::Baseline,
                             ..Default::default()
                         },
-                        color: Color::rgb(0.5, 0.5, 0.5).into(),
+                        color: NORMAL_BUTTON.into(),
                         ..Default::default()
-                    }).remove::<Interaction>()
-                    .insert(GuiTextBox { id: 3 - j as u32 })
+                    })
+                    .insert(GuiButtons {
+                        content: branch[j].clone(),
+                        id: j as u32,
+                    })
+                    .insert(GuiButtonId { id: 3 - j as u32 })
                     .with_children(|parent| {
-                        parent.spawn_bundle(TextBundle {
-                            text: Text::with_section(
-                                match &branch[j] { GuiOr::Id(e) => e.clone(), _ => panic!("you suck") },
-                                TextStyle {
-                                    font: asset_server.load("fonts/zekton-rg.ttf"),
-                                    font_size: 26.0,
-                                    color: Color::rgb(0.9, 0.9, 0.9),
+                        let mut margin = Rect::default();
+                        margin.bottom = Val::Px(114.0);
+                        parent
+                            .spawn_bundle(NodeBundle {
+                                style: Style {
+                                    size: Size::new(Val::Px(147.0), Val::Px(33.0)),
+                                    margin: margin,
+                                    // horizontally center child text
+                                    justify_content: JustifyContent::Center,
+                                    // vertically center child text
+                                    align_items: AlignItems::Baseline,
+                                    ..Default::default()
                                 },
-                                Default::default(),
-                            ),
-                            ..Default::default()
-                        }).insert(GuiTextId { id: j as u32 });
+                                color: Color::rgb(0.5, 0.5, 0.5).into(),
+                                ..Default::default()
+                            })
+                            .remove::<Interaction>()
+                            .insert(GuiTextBox { id: 3 - j as u32 })
+                            .with_children(|parent| {
+                                parent
+                                    .spawn_bundle(TextBundle {
+                                        text: Text::with_section(
+                                            match &branch[j] {
+                                                GuiOr::Id(e) => e.clone(),
+                                                _ => panic!("you suck"),
+                                            },
+                                            TextStyle {
+                                                font: asset_server.load("fonts/zekton-rg.ttf"),
+                                                font_size: 26.0,
+                                                color: Color::rgb(0.9, 0.9, 0.9),
+                                            },
+                                            Default::default(),
+                                        ),
+                                        ..Default::default()
+                                    })
+                                    .insert(GuiTextId { id: j as u32 });
+                            });
                     });
-                });
             }
         });
     info!("gui done");
