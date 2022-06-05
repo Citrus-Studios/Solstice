c6a243120ae78f0cc083f7ee8326a51c82caad5d
wahfl2 <wahflbeljihn@gmail.com>
2022-06-05 18:52:30 -0500

    Refactored gui lookup to use PHF

--- src/constants.rs
+++ src/constants.rs
@@ -1,8 +1,10 @@
 use std::{collections::HashMap, f32::consts::PI};
 
 use lazy_static::lazy_static;
+use phf::phf_map;
 use rand::{thread_rng, Rng};
 
+use crate::gui_branch_builder::GuiBranchBuilder;
 use crate::player_system::gui_system::gui::GuiOr;
 
 use bevy::math::Vec3;
@@ -20,105 +22,132 @@ pub const FABRICATOR_SPEED: u32 = 100;
 pub const MAX_BUILD_DISTANCE: f32 = 30.0;
 pub const SNAP_DISTANCE: f32 = 0.5;
 
-macro_rules! GUIBranch {
-    ($name:literal - $x0:literal $y0:literal, $x1:literal $y1:literal, $x2:literal $y2:literal, $x3:literal $y3:literal) => {
-        (
-            String::from($name),
-            [
-                if $x0 == 0 {
-                    if $y0 == "None" {
-                        GuiOr::None
-                    } else {
-                        GuiOr::Id(String::from($y0))
-                    }
-                } else {
-                    if $y0 == "None" {
-                        GuiOr::None
-                    } else {
-                        GuiOr::Item(String::from($y0))
-                    }
-                },
-                if $x1 == 0 {
-                    if $y1 == "None" {
-                        GuiOr::None
-                    } else {
-                        GuiOr::Id(String::from($y1))
-                    }
-                } else {
-                    if $y1 == "None" {
-                        GuiOr::None
-                    } else {
-                        GuiOr::Item(String::from($y1))
-                    }
-                },
-                if $x2 == 0 {
-                    if $y2 == "None" {
-                        GuiOr::None
-                    } else {
-                        GuiOr::Id(String::from($y2))
-                    }
-                } else {
-                    if $y2 == "None" {
-                        GuiOr::None
-                    } else {
-                        GuiOr::Item(String::from($y2))
-                    }
-                },
-                if $x3 == 0 {
-                    if $y3 == "None" {
-                        GuiOr::None
-                    } else {
-                        GuiOr::Id(String::from($y3))
-                    }
-                } else {
-                    if $y3 == "None" {
-                        GuiOr::None
-                    } else {
-                        GuiOr::Item(String::from($y3))
-                    }
-                },
-            ],
-        )
-    };
-}
+// NAMING: Everything BEFORE underscores (_) will be IGNORED in display text. Use spaces.
+pub const GUI_LOOKUP: phf::Map<&'static str, [GuiOr; 4]> = phf_map! {
+    "base" => GuiBranchBuilder::new()
+        .branch("Iridium")
+        .branch("Structures")
+        .branch("Military")
+        .branch("Technology")
+        .build(),
+
+    "Iridium" => GuiBranchBuilder::new()
+        .branch("Pipes")
+        .branch("Extractors")
+        .branch("Tanks")
+        .branch("Iridium_Special")
+        .build(),
+
+    "Structures" => GuiBranchBuilder::new()
+        .item("Seat")
+        .item("Bridge")
+        .branch("Structures_Defense")
+        .branch("Basic")
+        .build(),
+
+    "Military" => GuiBranchBuilder::new()
+        .item("Priority Beacon")
+        .item("Shield Generator")
+        .branch("Military_Production")
+        .branch("Weapons")
+        .build(),
+
+    "Technology" => GuiBranchBuilder::new()
+        .item("Spawn Point")
+        .item("Upgrade Station")
+        .branch("Structure Tech")
+        .branch("Technology_Misc")
+        .build(),
+
+    // Iridium
+    "Pipes" => GuiBranchBuilder::new()
+        .item("Pipe")
+        .item("T-Junction")
+        .item("Four-Way")
+        .item("Elbow")
+        .build(),
+
+    "Extractors" => GuiBranchBuilder::new()
+        .item("Well Pump")
+        .item("Condenser")
+        .item("Submersible")
+        .item("Crystal Resonator")
+        .build(),
+
+    "Tanks" => GuiBranchBuilder::new()
+        .item("Small Tank")
+        .item("Standard Tank")
+        .item("Large Tank")
+        .item("Reservoir")
+        .build(),
+
+    "Iridium_Special" => GuiBranchBuilder::new()
+        .item("Cap")
+        .item("Refill Station")
+        .branch("Iridium_Special_Tech")
+        .branch("Valves")
+        .build(),
+
+    "Iridium_Special_Tech" => GuiBranchBuilder::new()
+        .item("Distributor Cap")
+        .item("Gauge")
+        .build(),
+
+    "Valves" => GuiBranchBuilder::new()
+        .item("Valve")
+        .item("Drop Tank")
+        .build(),
+
+    // Structures
+    "Structures_Defense" => GuiBranchBuilder::new()
+        .item("Wall")
+        .item("Keep")
+        .item("Gate")
+        .build(),
+
+    "Basic" => GuiBranchBuilder::new()
+        .item("Ladder Block") // There are more, obviously
+        .build(),
+
+    // Military
+    "Military_Production" => GuiBranchBuilder::new()
+        .item("Arsenal")
+        .item("Garage")
+        .item("S.S.I.M.")
+        .build(),
+
+    "Weapons" => GuiBranchBuilder::new()
+        .branch("Offense")
+        .branch("Defense")
+        .build(),
+
+    "Offense" => GuiBranchBuilder::new()
+        .item("Artillery")
+        .item("Bore")
+        .build(),
+
+    "Defense" => GuiBranchBuilder::new()
+        .item("Turret")
+        .item("Arc Turret")
+        .item("Point Defense")
+        .build(),
+
+    // Technology
+    "Structure Tech" => GuiBranchBuilder::new()
+        .item("Fabricator")
+        .item("Automechanic")
+        .build(),
+
+    "Technology_Misc" => GuiBranchBuilder::new()
+        .item("Boost Pad")
+        .item("Distributor")
+        .item("Transceiver")
+        .item("Spatial Anchor")
+        .build(),
+};
 
 lazy_static! {
     pub static ref SEED: u32 = thread_rng().gen::<u32>();
-
-    pub static ref GUI_LOOKUP: HashMap<String, [GuiOr<String>; 4]> = HashMap::from([
-            // NAMING: Everything BEFORE underscores (_) will be IGNORED in display text. Use spaces.
-            GUIBranch!("base" - 0 "Iridium", 0 "Structures", 0 "Military", 0 "Technology"),
-
-            GUIBranch!("Iridium" - 0 "Pipes", 0 "Extractors", 0 "Tanks", 0 "Iridium_Special"),
-            GUIBranch!("Structures" - 1 "Seat", 1 "Bridge", 0 "Structures_Defense", 0 "Basic"),
-            GUIBranch!("Military" - 1 "Priority Beacon", 1 "Shield Generator", 0 "Military_Production", 0 "Weapons"),
-            GUIBranch!("Technology" - 1 "Spawn Point", 1 "Upgrade Station", 0 "Structure Tech", 0 "Technology_Misc"),
-
-            // Iridium
-            GUIBranch!("Pipes" - 1 "Pipe", 1 "T-Junction", 1 "Four-Way", 1 "Elbow"),
-            GUIBranch!("Extractors" - 1 "Well Pump", 1 "Condenser", 1 "Submersible", 1 "Crystal Resonator"),
-            GUIBranch!("Tanks" - 1 "Small Tank", 1 "Standard Tank", 1 "Large Tank", 1 "Reservoir"),
-            GUIBranch!("Iridium_Special" - 1 "Cap", 1 "Refill Station", 0 "Iridium_Special_Tech", 0 "Valves"),
-
-            GUIBranch!("Iridium_Special_Tech" - 1 "Distributor Cap", 1 "Gauge", 0 "None", 0 "None"),
-            GUIBranch!("Valves" - 1 "Valve", 1 "Drop Tank", 0 "None", 0 "None"),
-
-            // Structures
-            GUIBranch!("Defense_Structures" - 1 "Wall", 1 "Keep", 1 "Gate", 0 "None"),
-             /* make these later im lazy */
-            GUIBranch!("Basic" - 1 "Ladder Block", 0 "None", 0 "None", 0 "None"),
-
-            // Military
-            GUIBranch!("Military_Production" - 1 "Arsenal", 1 "Garage", 1 "S.S.I.M.", 0 "None"),
-            GUIBranch!("Weapons" - 0 "Offense", 0 "Defense", 0 "None", 0 "None"),
-
-            GUIBranch!("Offense" - 1 "Artillery", 1 "Bore", 0 "None", 0 "None"),
-            GUIBranch!("Defense" - 1 "Turret", 1 "Arc Turret", 1 "Point Defense", 0 "None"),
-
-            // Technology
-            GUIBranch!("Structure Tech" - 1 "Fabricator", 1 "Automechanic", 0 "None", 0 "None"),
-            GUIBranch!("Technology_Misc" - 1 "Boost Pad", 1 "Distributor", 1 "Transceiver", 1 "Spatial Anchor"),
-        ]);
 
     pub static ref GUI_BACK_LOOKUP: HashMap<String, String> = HashMap::from([
         ("base".ts(), "base".ts()),
