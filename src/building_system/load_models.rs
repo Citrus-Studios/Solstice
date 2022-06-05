6d3c5cee7a3f10dd5498c3f7b2ce813b16a10ff5
wahfl2 <wahflbeljihn@gmail.com>
2022-06-05 18:54:50 -0500

    Added more models and refactored to be more consistent

--- src/building_system/load_models.rs
+++ src/building_system/load_models.rs
@@ -1,13 +1,18 @@
-use bevy::{prelude::*, gltf::GltfMesh, asset::LoadState, ecs::schedule::ShouldRun};
+use bevy::{asset::LoadState, ecs::schedule::ShouldRun, gltf::GltfMesh, prelude::*};
 
 use super::ModelHandles;
 
 pub const NONE_HANDLE: Option<Handle<GltfMesh>> = None;
-pub const NUM_MODELS: usize = 2;
+pub const NUM_MODELS: usize = 7;
 
 const MODEL_PATHS: [&'static str; NUM_MODELS] = [
     "models/buildings/well_pump.gltf",
     "models/pipes/pipe_base.gltf",
+    "models/ground1/ground1.gltf",
+    "models/ground1/hollow_ground.gltf",
+    "models/ground1/spires_full.gltf",
+    "models/ground1/spires_hollow.gltf",
+    "models/ground1/well_ground.gltf",
 ];
 
 pub fn initiate_load(asset_server: Res<AssetServer>, mut model_handles: ResMut<ModelHandles>) {
@@ -19,11 +24,16 @@ pub fn initiate_load(asset_server: Res<AssetServer>, mut model_handles: ResMut<M
     info!("load done");
 }
 
-pub fn get_load_states(asset_server: Res<AssetServer>, model_handles: Res<ModelHandles>) -> ShouldRun {
+pub fn get_load_states(
+    asset_server: Res<AssetServer>,
+    model_handles: Res<ModelHandles>,
+) -> ShouldRun {
     for handle in model_handles.handles.clone() {
-        if asset_server.get_load_state(handle.unwrap()) != LoadState::Loaded {
-            return ShouldRun::NoAndCheckAgain
+        match asset_server.get_load_state(handle.as_ref().unwrap()) {
+            LoadState::Loaded => continue,
+            LoadState::Failed => warn!("Failed to load model, Handle: {:?}", handle.unwrap()),
+            _ => return ShouldRun::NoAndCheckAgain,
         }
     }
-    return ShouldRun::Yes
+    return ShouldRun::Yes;
 }
